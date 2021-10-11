108ac1905e719edfd54ec9fc214d10400e2f1e2c
tupta1999 <tupta1999@gmail.com>
2021-10-11 22:30:40 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//route
+app.get('/tin-tuc', (req, res) => {
+    var a = 1
+    var b = 2
+    var c = a + b
+    res.send('HELLO WORD')
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
